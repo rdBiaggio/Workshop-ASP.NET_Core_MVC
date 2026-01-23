4c04370a48bd12522ec6fbde789ebf78f1d37f62
Rafael Biaggio <rafaeldbiaggio@gmail.com>
2026-01-23 14:49:39 -0300

    Foreign Key not null

--- Models/Services/SellerService.cs
+++ Models/Services/SellerService.cs
@@ -19,6 +19,7 @@ namespace SalesWebMVC.Models.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
